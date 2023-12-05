24e4e7d198f871096e15727bb2887960d4388bff
Andrei G <118891797+andrei-g99@users.noreply.github.com>
2023-12-05 10:54:41 +0100

    Update render.py

--- render.py
+++ render.py
@@ -1,190 +1,92 @@
+import csv
+import numpy as np
 import os
 import config
-import numpy as np
-import mayavi.mlab as mlab
-import math
-import glob
-import vtk
-import pandas as pd
-from moviepy.editor import ImageSequenceClip
-from vtkmodules.vtkIOImage import (
-    vtkBMPWriter,
-    vtkJPEGWriter,
-    vtkPNGWriter,
-    vtkPNMWriter,
-    vtkPostScriptWriter,
-    vtkTIFFWriter
-)
-from vtkmodules.vtkRenderingCore import (
-    vtkActor,
-    vtkPolyDataMapper,
-    vtkRenderWindow,
-    vtkRenderWindowInteractor,
-    vtkRenderer,
-    vtkWindowToImageFilter
-)
 
-def WriteImage(fileName, renWin, rgba=True):
-    '''
-    Write the render window view to an image file.
+simulation_data = []
+simulation_steps = config.simulation_steps
+body_list = config.body_list
+init_box_length = config.init_box_length
+dt = config.dt
+N = config.N
+G = config.G
 
-    Image types supported are:
-     BMP, JPEG, PNM, PNG, PostScript, TIFF.
-    The default parameters are used for all writers, change as needed.
+for t in range(simulation_steps):
+    for i in body_list:
+        # Collision handler and bound check
+        if i.alive:
+            if np.linalg.norm(i.position_history[-1]) > init_box_length:
+                # Body out of sim bounds, eliminate from simulation
+                i.alive = False
+            else:
+                for j in body_list:
+                    if i != j and j.alive:
+                        distance_vector = j.position_history[-1] - i.position_history[-1]
+                        radius_sum = j.radius + i.radius
+                        if np.linalg.norm(distance_vector) < radius_sum:
+                            # Collision detected between body i and j
+                            # The body with smaller mass is eliminated
+                            total_mass = i.mass + j.mass
 
-    :param fileName: The file name, if no extension then PNG is assumed.
-    :param renWin: The render window.
-    :param rgba: Used to set the buffer type.
-    :return:
-    '''
+                            velocity_of_merger = ((i.mass)/(total_mass)) * i.velocity_history[-1] + ((j.mass)/(total_mass)) * j.velocity_history[-1]
+                            center_of_mass = ((i.mass)/(total_mass)) * i.position_history[-1] + ((j.mass)/(total_mass)) * j.position_history[-1]
+                            if i.mass <= j.mass:
+                                i.alive = False
+                                j.velocity_history[-1] = velocity_of_merger
+                                j.position_history[-1] = center_of_mass
 
-    import os
+                            else:
+                                j.alive = False
+                                i.velocity_history[-1] = velocity_of_merger
+                                i.position_history[-1] = center_of_mass
 
-    if fileName:
-        # Select the writer to use.
-        path, ext = os.path.splitext(fileName)
-        ext = ext.lower()
-        if not ext:
-            ext = '.png'
-            fileName = fileName + ext
-        if ext == '.bmp':
-            writer = vtkBMPWriter()
-        elif ext == '.jpg':
-            writer = vtkJPEGWriter()
-        elif ext == '.pnm':
-            writer = vtkPNMWriter()
-        elif ext == '.ps':
-            if rgba:
-                rgba = False
-            writer = vtkPostScriptWriter()
-        elif ext == '.tiff':
-            writer = vtkTIFFWriter()
-        else:
-            writer = vtkPNGWriter()
+    for i in body_list:
+        # Propagate gravity dynamics
+        if i.alive:
+            for j in body_list:
+                if i != j and j.alive:
+                    distance_vector = j.position_history[-1] - i.position_history[-1]
+                    acceleration_on_i = distance_vector * ((G * j.mass) / (np.linalg.norm(distance_vector) ** 3))
 
-        windowto_image_filter = vtkWindowToImageFilter()
-        windowto_image_filter.SetInput(renWin)
-        windowto_image_filter.SetScale(1)  # image quality
-        if rgba:
-            windowto_image_filter.SetInputBufferTypeToRGBA()
-        else:
-            windowto_image_filter.SetInputBufferTypeToRGB()
-            # Read from the front buffer.
-            windowto_image_filter.ReadFrontBufferOff()
-            windowto_image_filter.Update()
+                    velocity = i.velocity_history[-1] + acceleration_on_i * dt
+                    i.velocity_history.append(velocity)
 
-        writer.SetFileName(fileName)
-        writer.SetInputConnection(windowto_image_filter.GetOutputPort())
-        writer.Write()
-    else:
-        raise RuntimeError('Need a filename.')
+                    position = i.position_history[-1] + velocity * dt
+                    i.position_history.append(position)
 
+    # Collecting data at the current timestep
+    timestep_data = []
+    timestep_data.append(t)
+    timestep_data.append(N)
+    for body in body_list:
+        #timestep_data.append(particle.position_history[-1].tolist() + particle.velocity_history[-1].tolist() + [particle.mass])
+        timestep_data.append(body.position_history[-1][0])
+        timestep_data.append(body.position_history[-1][1])
+        timestep_data.append(body.position_history[-1][2])
+        timestep_data.append(body.velocity_history[-1][0])
+        timestep_data.append(body.velocity_history[-1][1])
+        timestep_data.append(body.velocity_history[-1][2])
+        timestep_data.append(body.mass)
+        timestep_data.append(1)
 
-data = pd.read_csv('simulation_data.csv')
+    simulation_data.append(timestep_data)
 
-# Controlling FPS and time: FPS * video_duration(seconds) = sim_timesteps
-# e.g. FPS = 60 and video_duration = 10
-# sim_timesteps = 600, we need to sample only 600 timesteps regardless of how many the simulation provides!
-
-FPS = config.FPS
-video_duration = config.video_duration
-steps_to_sample = FPS*video_duration
-total_timesteps = len(data['timestep'])
-
-if steps_to_sample > total_timesteps:
-    steps_to_sample = total_timesteps
-
-sampling_cnt_threshold = math.floor(total_timesteps/steps_to_sample)
-
-# Initialize vtk renderer etc.
-renderer = vtk.vtkRenderer()
-renderWindow = vtk.vtkRenderWindow()
-renderWindow.AddRenderer(renderer)
-
-renderWindowInteractor = vtk.vtkRenderWindowInteractor()
-renderWindowInteractor.SetRenderWindow(renderWindow)
-
-#Set camera
-camera = vtk.vtkCamera()
-camera.SetPosition(config.camera_position[0], config.camera_position[1], config.camera_position[2])
-camera.SetFocalPoint(config.camera_direction[0], config.camera_direction[1], config.camera_direction[2])
-renderer.SetActiveCamera(camera)
-
-# Sphere source for glyphs
-sphereSource = vtk.vtkSphereSource()
-sphereSource.SetRadius(config.spheres_radius)  # Set the radius of the spheres
-
-frames = []
-
-# Add 3D points
-# Example: points.InsertNextPoint(1.0, 2.0, 3.0)
-cnt = 1
-for index, row in data.iterrows():
-    if cnt == sampling_cnt_threshold:
-        cnt = 1
-        points = vtk.vtkPoints()
-        N = row['number_of_bodies']
-        timestep = row['timestep']
-        for i in range(int(N)):
-            if row[f'body_{i}_status']:
-                x = float(row[f'body_{i}_pos_x'])
-                y = float(row[f'body_{i}_pos_y'])
-                z = float(row[f'body_{i}_pos_z'])
-                m = float(row[f'body_{i}_mass'])
-
-                points.InsertNextPoint(x, y, z)
-        # Access data from each row with row['column_name']
-        # For example, row['name'] or row['age'] if your columns are named 'name' and 'age'
-
-        polydata = vtk.vtkPolyData()
-        polydata.SetPoints(points)
-
-        glyphFilter = vtk.vtkGlyph3D()
-        glyphFilter.SetSourceConnection(sphereSource.GetOutputPort())
-        glyphFilter.SetInputData(polydata)
-        glyphFilter.Update()
-
-        mapper = vtk.vtkPolyDataMapper()
-        mapper.SetInputConnection(glyphFilter.GetOutputPort())
-
-        actor = vtk.vtkActor()
-        actor.SetMapper(mapper)
-        actor.GetProperty().SetColor(config.sphere_color[0], config.sphere_color[1], config.sphere_color[2])
-        renderer.AddActor(actor)
-
-        renderer.SetBackground(config.background_color[0], config.background_color[1], config.background_color[2])  # RGB background color
-        renderWindow.SetSize(config.window_resolution[0], config.window_resolution[1])  # Window size
-        # Render and save frame
-        renderWindow.Render()
-        frame_filename = f"frame_{timestep}.png"
-        WriteImage(frame_filename, renderWindow, rgba=True)
-        frames.append(frame_filename)
-
-        # Remove actor to prepare for next timestep
-        renderer.RemoveActor(actor)
-
-    cnt += 1
-
-file_path = f'{config.video_filename}.mp4'  # Replace with your file path
+file_path = f'{config.data_filename}.csv'  # Replace with your file path
 try:
     os.remove(file_path)
 except FileNotFoundError:
     print('Creating new file')
 
-# Compile frames into a video
-clip = ImageSequenceClip(frames, fps=FPS)  # fps can be adjusted
-clip.write_videofile(f'{config.video_filename}.mp4', codec="libx264")
-renderWindowInteractor.Start()
+with open(file_path, 'w', newline='') as file:
+    writer = csv.writer(file)
+    # Write a header row (optional, depending on your data structure)
+    header = ['timestep', 'number_of_bodies']
+    for i in range(len(body_list)):
+        header += [f'body_{i}_pos_x', f'body_{i}_pos_y', f'body_{i}_pos_z',
+                   f'body_{i}_vel_x', f'body_{i}_vel_y', f'body_{i}_vel_z', f'body_{i}_mass', f'body_{i}_status']
+    writer.writerow(header)
 
-# Path where the files are located (assuming current directory for this example)
-path = '.'
-
-# Pattern to match files of the format 'frame_{number}.png'
-pattern = os.path.join(path, 'frame_*')
-
-# Find all files matching the pattern
-files_to_delete = glob.glob(pattern)
-
-# Delete the files
-for file in files_to_delete:
-    os.remove(file)
+    # Write the data
+    for row in simulation_data:
+        writer.writerow(row)
+    print(f'{config.data_filename}.csv has been generated')
