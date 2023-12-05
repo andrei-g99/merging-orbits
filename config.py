8e626ef7e176fc8c4fe3a0c16748bb2f86db98d1
Andrei G <118891797+andrei-g99@users.noreply.github.com>
2023-12-05 10:54:18 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# SIM CONFIGS
 class Body:
     def __init__(self, initial_position, initial_velocity, mass, alive_status=True, radius = 10):
         self.position_history = [initial_position]
@@ -33,3 +34,21 @@ body_list.append(Body(np.array([0, 100, 0]), np.array([1, 0, 0]), 10, True))
 body_list.append(Body(np.array([0, 0, 0]), np.array([0, 0, 0]), 100, True))
 dt = 0.01
 simulation_steps = 50000
+
+data_filename = 'sim_data'
+
+# END SIM CONFIGS
+
+# RENDERING CONFIGS
+
+FPS = 60
+video_duration = 10 # Seconds
+camera_position = [-500, 0, 500] # Camera absolute position
+camera_direction = [0, 0, 0] # Pointing direction (focal point)
+spheres_radius = 10
+sphere_color = [1, 1, 1] # RGB spheres color
+background_color = [0, 0, 0] # RGB background color
+window_resolution = [1920, 1080] # Pixel resolution width x height
+
+video_filename = 'simrun'
+# END RENDER CONFIGS
